fdcf72f45ac0eef2be3bcf62c44f095a946aeaf1
sanba <sanba0519@qq.com>
2026-08-09 02:09:08 +0800

    Fix: fix 语法错误

--- .github/bot.py
+++ .github/bot.py
@@ -66,5 +66,3 @@ if __name__ == '__main__':
             sys.exit(1)
     else:
         print("[-] No file paths provided as arguments.")
-    else:
-        print("[-] No file paths provided as arguments.")
